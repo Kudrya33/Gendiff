40349be0e9592b652a168e773be212dc76aec393
Павел Кудря <Skelger@mail.ru>
2024-05-21 00:20:00 +0300

    update DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -13,16 +13,14 @@ public class DifferTest {
     @Test
     public void testDiffer() throws Exception {
         String format = "stylish";
-        Path first = Paths.get(filepath1).toAbsolutePath().normalize();
-        Path last = Paths.get(filepath2).toAbsolutePath().normalize();
-        String expected = "{\n" +
-                " - follow: false\n" +
-                "   host: hexlet.io\n" +
-                " - proxy: 123.234.53.22\n" +
-                " - timeout: 50\n" +
-                " + timeout: 20\n" +
-                " + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+                + " - follow: false\n"
+                + "   host: hexlet.io\n"
+                + " - proxy: 123.234.53.22\n"
+                + " - timeout: 50\n"
+                + " + timeout: 20\n"
+                + " + verbose: true\n"
+                + "}";
         var actual = Differ.generate(filepath1, filepath2, format);
         assertEquals(expected, actual);
     }
